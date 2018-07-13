f9d4754a5f5c377b4db42c265aacf50dff0790f5
Ravindar Negi <ravindar.negi@outlook.com>
2018-07-13 23:13:34 +0530

    removed so many comments

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,14 +12,14 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     #set the background color.
     bg_color = (230,230,230)
-    #Start the main loop for the game.
+
     while True:
-        #Watch the keyboard and mouse events.
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             screen.fill(ai_settings.bg_color)
-        #Make the most recently drawn screen visible.
+        
         pygame.display.flip()
 # Added new comment
 run_game()
