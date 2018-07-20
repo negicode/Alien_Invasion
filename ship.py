58465e76b0164d3aefdf49ae516354a9b0d73943
Ravindar Negi <ravindar.negi@outlook.com>
2018-07-20 05:57:33 +0530

    added jet images

--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship():
         self.screen = screen
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/jet1.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
